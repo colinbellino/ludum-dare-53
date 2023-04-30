7646a5fb76cacca7c9278dd83779f94d59b5958c
API-Beast <api.beast@gmail.com>
2023-04-30 10:17:06 +0200

    GameData: Prefill scene properties with those defined in base class

--- scripts/autoload/GameData.gd
+++ scripts/autoload/GameData.gd
@@ -2,13 +2,15 @@ extends Node
 
 var scene_properties := {}
 var scene_by_category := {}
+var category_defaults := {}
 
 func _ready():
-	add_category("Turrets")
+	add_category("Turrets", BaseTurret)
 	add_to_db("Turrets", preload("res://game/cart/LightMGTurret.tscn"))
 
 func add_to_db(category:String, scene:PackedScene):
-	var properties = {}
+	var properties = category_defaults[category].duplicate()
+	# Scene overrides
 	var scene_state = scene.get_state()
 	var node_id = 0 # We just assume that the first node inside the scene file is the root.
 	var num_properties = scene_state.get_node_property_count(node_id)
@@ -18,6 +20,11 @@ func add_to_db(category:String, scene:PackedScene):
 	scene_properties[scene] = properties
 	scene_by_category[category].push_back(scene)
 
-func add_category(category:String):
+func add_category(category:String, base_class:Script):
 	scene_by_category[category] = []
+	var defaults = {}
+	for p in base_class.get_script_property_list():
+		if (p.usage & PROPERTY_USAGE_SCRIPT_VARIABLE) and (p.usage & PROPERTY_USAGE_STORAGE):
+			defaults[p.name] = base_class.get_property_default_value(p.name)
+	category_defaults[category] = defaults
 
