bea6c7190fe8a5d4b62a4931519eb1982fef1318
Colin Bellino <git+ld53@colinbellino.com>
2023-05-02 00:24:29 +0200

    Hide HUD during title

--- game/level/HUD.gd
+++ game/level/HUD.gd
@@ -2,8 +2,5 @@ extends Panel
 
 func _process(_delta):
 	if GameData.level:
-		visible = true
 		%Money.text = "Currency: %s" % [GameData.money]
 		%Wave.text = "Wave: %s" % [GameData.level.wave_index]
-	else:
-		visible = false
