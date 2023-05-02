11e6229f9edcef24989fa35ab6d2327a371978f2
Colin Bellino <git+ld53@colinbellino.com>
2023-05-02 02:23:18 +0200

    Disable spatial audio to fix all issues

--- scripts/autoload/AudioPlayer.gd
+++ scripts/autoload/AudioPlayer.gd
@@ -3,9 +3,9 @@ extends Node2D
 var bus_main : int
 var bus_music : int
 var bus_sound : int
-var audio_player_sound_one_shot : AudioStreamPlayer2D
-var audio_player_sound : AudioStreamPlayer2D
-var audio_player_music : AudioStreamPlayer2D
+var audio_player_sound_one_shot : AudioStreamPlayer
+var audio_player_sound : AudioStreamPlayer
+var audio_player_music : AudioStreamPlayer
 
 func _ready():
 	bus_main = AudioServer.get_bus_index("Master")
@@ -23,17 +23,16 @@ func _ready():
 
 func _process(_delta):
 	if GameData.level && GameData.level.ship:
-		self.global_position = GameData.level.ship.global_position
+		# self.global_position = GameData.level.ship.global_position
 
 		audio_player_music.pitch_scale = Engine.get_time_scale()
 
-func play_sound(stream: AudioStream, sound_position: Vector2 = get_viewport_rect().size / 2, loop: bool = false, pitch_scale : float = 1.0) -> void:
+func play_sound(stream: AudioStream, sound_position: Vector2 = position, loop: bool = false, pitch_scale : float = 1.0) -> void:
 	if stream is AudioStreamWAV == false:
 		stream.loop = loop
 
 	var player := spawn_audio_player()
-	player.position = position
-	player.position = sound_position
+	# player.position = sound_position
 	player.pitch_scale = pitch_scale
 	player.stream = stream
 	player.play()
@@ -47,18 +46,18 @@ func play_sound_random(streams: Array, sound_position: Vector2, loop: bool = fal
 
 func play_music(stream: AudioStream, loop: bool = true) -> void:
 	stream.set_loop(loop)
+	print("play_music", stream)
 
 	audio_player_music.stream = stream
 	if audio_player_music.playing:
 		audio_player_music.stop()
 	audio_player_music.stream = stream
-	audio_player_music.pitch_scale = Engine.time_scale
 	audio_player_music.play()
 
 func stop_music() -> void:
 	audio_player_music.stop()
 
-func spawn_audio_player() -> AudioStreamPlayer2D:
+func spawn_audio_player() -> AudioStreamPlayer:
 	var audio_player = audio_player_sound_one_shot.duplicate()
 	if Engine.time_scale > 0:
 		audio_player.pitch_scale = Engine.time_scale
