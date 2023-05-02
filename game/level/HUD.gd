3201b1f5243f1b2d4c6878408a5ef23060cf0fb4
Colin Bellino <git+ld53@colinbellino.com>
2023-05-02 19:26:54 +0200

    Fix missing translation

--- game/level/HUD.gd
+++ game/level/HUD.gd
@@ -2,5 +2,5 @@ extends Panel
 
 func _process(_delta):
 	if GameData.level:
-		%Money.text = "Currency: %s" % [GameData.money]
+		%Money.text = tr("Currency: %s") % [GameData.money]
 		%Wave.text = "Wave: %s" % [GameData.level.wave_index]
