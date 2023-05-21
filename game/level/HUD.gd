a3ec45927a303b05414b8c97f8087140e3f408de
Colin Bellino <git+ld53@colinbellino.com>
2023-05-21 16:04:38 +0200

    Add pause button

--- game/level/HUD.gd
+++ game/level/HUD.gd
@@ -1,6 +1,15 @@
-extends Panel
+class_name HUD extends Control
 
-func _process(_delta):
+var button_pause: Button
+
+func _ready() -> void:
+	button_pause = get_node("%Pause")
+	button_pause.connect("pressed", button_pause_pressed)
+
+func _process(_delta: float) -> void:
 	if GameData.level:
 		%Money.text = tr("Currency: %d") % [GameData.money]
 		%Wave.text = "Wave: %s" % [GameData.level.wave_index]
+
+func button_pause_pressed() -> void:
+	Overlay.show_modal(preload("res://game/main_menu/PauseUI.tscn"))
