5108299574bb4a3dad30401f552281d49f30b664
Colin Bellino <git+ld53@colinbellino.com>
2023-05-30 18:49:36 +0200

    Click on a world node to start the level state

--- game/world_map/WorldMapUI.gd
+++ game/world_map/WorldMapUI.gd
@@ -5,9 +5,16 @@ class WorldNode:
 	var position: Vector2
 	var color: Color
 	var children: Array[WorldNode]
+	var rect: Rect2
+	var size: float
+
+class DrawCall:
+	var layer: int
+	var proc
 
 const OFFSET := Vector2(480, 50)
 const SCALE := Vector2(80, 130)
+const RECT_SIZE := 48
 const colors : Array[Color] = [
 	Color.RED,
 	Color.PURPLE,
@@ -50,47 +57,22 @@ const names : Array[String] = [
 var root : WorldNode
 var font : Font
 var button_skip : Button
+var draw_calls : Array[DrawCall]
+var selected_node: WorldNode
 
 func _ready() -> void:
 	button_skip = get_node("%Skip")
 	button_skip.connect("pressed", button_skip_pressed)
 	font = (get_node("Label") as Label).get_theme_font("font")
 
-	root = WorldNode.new()
-	root.name = "Boss"
-	root.position = Vector2(0, 0) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	root.color = colors[randi_range(0, colors.size() - 1 )]
-
-	var node1 = WorldNode.new()
-	node1.name = names[randi_range(0, names.size() - 1)]
-	node1.position = Vector2(-1, 1) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	node1.color = colors[randi_range(0, colors.size() - 1 )]
-	var node2 = WorldNode.new()
-	node2.name = names[randi_range(0, names.size() - 1)]
-	node2.position = Vector2(1, 1) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	node2.color = colors[randi_range(0, colors.size() - 1 )]
-
-	var node3 = WorldNode.new()
-	node3.name = names[randi_range(0, names.size() - 1)]
-	node3.position = Vector2(-2, 2) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	node3.color = colors[randi_range(0, colors.size() - 1 )]
-	var node4 = WorldNode.new()
-	node4.name = names[randi_range(0, names.size() - 1)]
-	node4.position = Vector2(0, 2) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	node4.color = colors[randi_range(0, colors.size() - 1 )]
-	var node5 = WorldNode.new()
-	node5.name = names[randi_range(0, names.size() - 1)]
-	node5.position = Vector2(2, 2) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	node5.color = colors[randi_range(0, colors.size() - 1 )]
-
-	var start1 = WorldNode.new()
-	start1.name = names[randi_range(0, names.size() - 1)]
-	start1.position = Vector2(-1, 3) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	start1.color = colors[randi_range(0, colors.size() - 1 )]
-	var start2 = WorldNode.new()
-	start2.name = names[randi_range(0, names.size() - 1)]
-	start2.position = Vector2(1, 3) + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))
-	start2.color = colors[randi_range(0, colors.size() - 1 )]
+	root = make_world_node(Vector2(0, 0), "Boss")
+	var node1 = make_world_node(Vector2(-1, 1))
+	var node2 = make_world_node(Vector2(1, 1))
+	var node3 = make_world_node(Vector2(-2, 2))
+	var node4 = make_world_node(Vector2(0, 2))
+	var node5 = make_world_node(Vector2(2, 2))
+	var start1 = make_world_node(Vector2(-1, 3))
+	var start2 = make_world_node(Vector2(1, 3))
 
 	connect_node(root, node1)
 	connect_node(root, node2)
@@ -103,31 +85,65 @@ func _ready() -> void:
 	connect_node(node4, start2)
 	connect_node(node5, start2)
 
+func _process(_delta: float) -> void:
 	queue_redraw()
 
-func _process(delta: float) -> void:
-	# queue_redraw()
+	if Input.is_action_just_released("mouse_left"):
+		Overlay.transition(Res.SCENE_LEVEL)
 
 	if Input.is_action_just_released("debug_1"):
 		Overlay.transition(Res.SCENE_WORLD_MAP)
 
 func _draw() -> void:
 	var nodes : Array[WorldNode] = [root]
+	selected_node = null
 	while nodes.size() > 0:
 		var current = nodes.pop_front()
-		draw_circle(to_draw_position(current.position), 10, current.color)
-		draw_string_outline(font, to_draw_position(current.position) + Vector2(-5 * current.name.length(), 30), current.name, HORIZONTAL_ALIGNMENT_CENTER, -1, 16, 10, Color.BLACK)
-		draw_string(font, to_draw_position(current.position) + Vector2(-5 * current.name.length(), 30), current.name, HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.WHITE)
+		var selected : bool = current.rect.has_point(get_viewport().get_mouse_position())
+		if selected:
+			selected_node = current
+			var center : Vector2 = current.position + Vector2(-5 * current.name.length(), 30)
+			add_draw_call(func(): draw_circle(current.position, current.size + 2, Color.WHITE), 0)
+			add_draw_call(func(): draw_rect(Rect2(center.x, center.y, current.name.length() * 10 + 20, 30), Color(0.2, 0.2, 0.2, 1)), 3)
+			add_draw_call(func(): draw_string(font, center + Vector2(15, 20), current.name, HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.WHITE), 4)
+
+		add_draw_call(func(): draw_circle(current.position, current.size, current.color), 1)
+		# add_draw_call(func(): draw_rect(current.rect, Color(1, 1, 1, 0.1)), 1)
+
 		for child in current.children:
-			if child != null:
-				draw_line(to_draw_position(current.position), to_draw_position(child.position), Color.WHITE, 1)
-				nodes.push_back(child)
+			add_draw_call(func(): draw_line(current.position, child.position, Color.WHITE, 1))
+			nodes.push_back(child)
+
+	draw_calls.sort_custom(custom_array_sort)
+	for draw_call in draw_calls:
+		draw_call.proc.call()
+
+	draw_calls.clear()
+
+func add_draw_call(proc, layer : int = 0) -> void:
+	var draw_call := DrawCall.new()
+	draw_call.proc = proc
+	draw_call.layer = layer
+	draw_calls.push_back(draw_call)
+
+func custom_array_sort(a: DrawCall, b: DrawCall) -> bool:
+	return a.layer < b.layer
 
 func button_skip_pressed() -> void:
 	Overlay.transition(Res.SCENE_LEVEL)
 
-func connect_node(from: WorldNode, to: WorldNode) -> void:
-	from.children.append(to)
+static func make_world_node(position: Vector2, node_name: String = "") -> WorldNode:
+	var node = WorldNode.new()
+	var rect_size := Vector2(RECT_SIZE, RECT_SIZE)
+	if node_name == "":
+		node.name = names[randi_range(0, names.size() - 1)]
+	else:
+		node.name = node_name
+	node.position = (position + Vector2(randf_range(-0.2, 0.2), randf_range(-0.1, 0.1))) * SCALE + OFFSET
+	node.color = colors[randi_range(0, colors.size() - 1 )]
+	node.rect = Rect2(node.position - rect_size / 2, rect_size)
+	node.size = randf_range(10, 15)
+	return node
 
-func to_draw_position(position: Vector2) -> Vector2:
-	return position * SCALE + OFFSET
+static func connect_node(from: WorldNode, to: WorldNode) -> void:
+	from.children.append(to)
